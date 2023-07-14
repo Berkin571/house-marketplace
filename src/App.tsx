ed4583fe71cabf85d95b2986a530d1e8c72d6182
bilgin <Berkin.Bilgin@adesso.de>
2023-07-14 11:29:42 +0200

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  Explore,
+  Offers,
+  Profile,
+  SignIn,
+  SignUp,
+  ForgotPassword,
+} from "./pages";
 
 function App() {
-  return <>Hello World</>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/angebote" element={<Offers />} />
+          <Route path="/mein-profil" element={<Profile />} />
+          <Route path="/anmelden" element={<SignIn />} />
+          <Route path="/registrieren" element={<SignUp />} />
+          <Route path="/passwort-vergessen" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
+      {/* TODO: Navbar */}
+    </>
+  );
 }
 
 export default App;
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,2 +1,6 @@
 export { Explore } from "./explore";
 export { Offers } from "./offers";
+export { Profile } from "./profile";
+export { SignIn } from "./signIn";
+export { SignUp } from "./signUp";
+export { ForgotPassword } from "./forgotPassword";
